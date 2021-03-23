122da38fbb67ea5ed0069b6b7f31e282015614e4
Dilthey Aislan <diltheyaislan@gmail.com>
2021-03-23 20:58:46 -0300

    index client refactoring

--- hacker-chat/client/index.js
+++
@@ -1,8 +0,0 @@
-import Events from 'events';
-import TerminalController from './src/terminalController.js';
-
-const componentEmitter = new Events();
-
-const controller = new TerminalController();
-
-await controller.initalizeTable(componentEmitter);
